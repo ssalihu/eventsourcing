ef4528e43ecbbd4f3f946a4e282576a5a1edff07
Safir Salihu <msafirs@gmail.com>
2019-01-17 17:35:03 -0600

    Reordering and Comments added

--- event-source.rb
+++ event-source.rb
@@ -4,54 +4,53 @@ require 'uuid'
 require 'json'
 
 def create_event_store_connection
+   
    client = HttpEventStore::Connection.new do |config|
-  # default value is '127.0.0.1'
-  config.endpoint = '127.0.0.1'
-  # default value is 2113
-  config.port = '2113'
-  # default value is 20 entries per page
-  config.page_size = '20'
+     config.endpoint = '127.0.0.1'
+     config.port = '2113'
+     config.page_size = '20'
    end
+   
    client
 end
 
 def generate_uuid()
-   uuid = UUID.new.generate 
-   uuid
+   UUID.new.generate 
 end
 
 
 stream_name = "order_1"
-
-client = create_event_store_connection
-data = {"data" => "order created"}
 EventData = Struct.new(:data, :event_type, :uuid)
+client = create_event_store_connection
 
-
+# Create First Event
+data = {"data" => "order created"}
 ed = EventData.new(data, "OrderCreated", generate_uuid)
-
-expected_version = 1
+expected_version = 1 # Optimistic lock needs
 client.append_to_stream(stream_name, ed)
 
+#Create Second event
 ed = EventData.new(data, "OrderPending", generate_uuid)
 client.append_to_stream(stream_name, ed)
 
+#Create a batched event using the same UUID.
 uuid = generate_uuid
-
 data = {"data" => "batch 1"}
 ed1 = EventData.new(data, "OrderPending-StillValidating", uuid)
 data = {"data" => "batch 2"}
 ed2 = EventData.new(data, "OrderPending-AmountLarge", uuid)
-
 eds = [ed1,ed2]
 client.append_to_stream(stream_name, eds)
 
-
+#Read and display all events forward
 p client.read_all_events_forward(stream_name)
+
+#Read and display events from a point
 start = 0
 count = 1
 p client.read_events_forward(stream_name, start, count)
 
+#Deleting a stream. Soft delete(all events are gone but can generate the stream back) and hard delete (returns 401 after this)
 hard_delete = false
 
 #client.delete_stream(stream_name, hard_delete)
